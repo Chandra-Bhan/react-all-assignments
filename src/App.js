b7a82fa35ab43a9df9112de8e9d5a35ee8228aeb
Chandra-Bhan-Prajapati <prajapatichandrabhan333@gmail.com>
2023-04-11 23:47:18 +0530

    Master React Dummy App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import EventInvokeTime from './Assignment2/EventInvokeTime';
 
 function App() {
   return (
     <div className="App">
       
-      <EventInvokeTime/>
+    
     </div>
   );
 }
