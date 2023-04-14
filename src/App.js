5617921095430dca16681726411fcc10b016667c
Chandra-Bhan-Prajapati <prajapatichandrabhan333@gmail.com>
2023-04-14 20:25:13 +0530

    React Day5 Assignment8 in working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Create from './pages/Create';
+import Update from './pages/Update';
+import Delete from './pages/Delete';
+import Navbar from './navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+       
+          <Route path='/' element={<Home/>}/>
+          <Route path='/create' element={<Create/>}/>
+          <Route path='/update' element={<Update/>}/>
+          <Route path='/delete' element={<Delete/>}/>
+        </Routes>
+      </BrowserRouter>
     
     </div>
   );
---
+++ src/pages/Delete.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Delete() {
+  return (
+    <div>
+      <h1>This is the Delete Page.</h1>
+    </div>
+  );
+}
+
+export default Delete;
