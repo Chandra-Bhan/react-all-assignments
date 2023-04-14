3cccb5036e2450fa2546c1edc627a5041a828015
Chandra-Bhan-Prajapati <prajapatichandrabhan333@gmail.com>
2023-04-14 19:34:27 +0530

    React Day5 Assignment8 react_router_make_two_pages_use_link_component_also is completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Navbar from './navbar/Navbar';
+
+
+
+// Create a simple React app with React Router. Set up two routes: one for
+//  the home page and one for a contact page. Use React Router's Link 
+//  component to navigate between the two pages.
+
+
 
 function App() {
   return (
     <div className="App">
       
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+     
+        <Route path='/' element={<Home/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+    </BrowserRouter>
     
     </div>
   );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+    <h2>React Day5 Assignment 8 React_router_make_two_pages_use_link_component_also</h2>
+      <h1>This is the home page</h1>
+    </div>
+  );
+}
+
+export default Home;
