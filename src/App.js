e7f531b35c7206a8270beaaacd32a47e4964fb01
Chandra-Bhan-Prajapati <prajapatichandrabhan333@gmail.com>
2023-04-24 16:47:18 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Todos from "./todos/Todos";
+import { Provider } from "react-redux";
+import { state } from ".";
 
 function App() {
   return (
     <div className="App">
-      
-    
+      {/* <Provider state={state}>
+        <Todos />
+      </Provider> */}
     </div>
   );
 }
