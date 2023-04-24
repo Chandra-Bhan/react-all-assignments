a73b8b86bd86df07bf6130239ffbc48997c490a4
Chandra-Bhan-Prajapati <prajapatichandrabhan333@gmail.com>
2023-04-24 15:13:24 +0530

    Completed the react Redux assignment 1 Make a counter application using Redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Counter from "./counter/Counter";
+import { Provider } from "react-redux";
+import { store } from ".";
 
 function App() {
   return (
     <div className="App">
-      
-    
+      <Provider store={store}>
+        <Counter />
+      </Provider>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
- 
-    <App />
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    incremented: (state) => {
+      state.value += 1;
+    },
+    decremented: (state) => {
+      state.value -= 1;
+    },
+    reset: (state) => {
+      state.value = 0;
+    },
+  },
+});
 
-);
+export const store = configureStore({
+  reducer: counterSlice.reducer,
+});
+
+// store.subscribe(() => console.log(store.getState()));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
